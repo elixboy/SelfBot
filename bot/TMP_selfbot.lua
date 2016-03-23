70b0eaf37dcbee2d0e83b304713181d5956f628b
elixboy <alireza.m.h.m98@gmail.com>
2016-03-23 14:38:38 +0430

    Update TMP_selfbot.lua

--- bot/TMP_selfbot.lua
+++ bot/TMP_selfbot.lua
@@ -225,30 +225,10 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {PutYourID(BOTID),tonumber(our_id)},--Sudo users
+    sudo_users = {207999111,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Our team!
-Alphonse (@Iwals)
-I M /-\ N (@Imandaneshi)
-Siyanew (@Siyanew)
-Rondoozle (@Potus)
-Seyedan (@Seyedan25)
-
-Special thanks to:
-Juan Potato
-Siyanew
-Topkecleon
-Vamptacus
-
-Our channels:
-English: @TeleSeedCH
-Persian: @IranSeed
+    about_text = [[کیرمم نی
 ]],
     help_text_realm = [[
 Realm Commands:
